cf33fcb6a86b817e9322b7d2a94e8ac67a564d61
arthuraraujobyjus <100610370+arthuraraujobyjus@users.noreply.github.com>
2022-08-08 14:46:13 -0300

    Add files via upload
    08.08.2022

--- sketch.js
+++ sketch.js
@@ -18,11 +18,37 @@ var magali;
 var sansao;
 var socrates;
 var aristoteles;
+var goku;
+var vegeta;
+var gohan;
+var mestreKame;
+var kuririn;
+var raditz;
+var bills;
+var majinBoo;
+var numero18;
+var titi;
 
 function preload(){
   papeldeparede = loadImage("./Imagens/background.png");
   magali = loadImage("./Imagens/melon.png");
   sansao = loadImage("./Imagens/Rabbit-01.png");
+  goku = loadAnimation("./Imagens/blink_1.png","./Imagens/blink_2.png","./Imagens/blink_3.png");
+  vegeta = loadAnimation("./Imagens/eat_0.png","./Imagens/eat_1.png","./Imagens/eat_2.png","./Imagens/eat_3.png","./Imagens/eat_4.png");
+  gohan = loadAnimation("./Imagens/sad_1.png","./Imagens/sad_2.png","./Imagens/sad_3.png",);
+  mestreKame = loadSound("./Sons/sound1.mp3");
+  kuririn = loadSound("./Sons/rope_cut.mp3");
+  raditz = loadSound("./Sons/sad.wav");
+  bills = loadSound("./Sons/eating_sound.mp3");
+  majinBoo = loadSound("./Sons/air.wav");
+
+  goku.playing = true;
+  vegeta.playing = true;
+  gohan.playing = true;
+
+  goku.looping = true;
+  vegeta.looping = false;
+  gohan.looping = false;
 }
 
 function setup() 
@@ -30,24 +56,43 @@ function setup()
   createCanvas(500,700);
   engine = Engine.create();
   world = engine.world;
+
+  mestreKame.play();
+  mestreKame.setVolume(0.4);
  
   rectMode(CENTER);
   ellipseMode(RADIUS);
   imageMode(CENTER);
   textSize(50);
 
+  goku.frameDelay = 15;
+  vegeta.frameDelay = 15;
+
   piso = new Piso(200,690,600,20);
   teiadomiranha=new Rope(6,{x:245,y:30});
   lemon=Bodies.circle(300,300,15);
   Matter.Composite.add(teiadomiranha.body,lemon);
   colabastao=new Fitacrepe(teiadomiranha,lemon);
-  socrates=createSprite(250,620,100,100);
+  socrates=createSprite(420,590,100,100);
   socrates.addImage(sansao);
+  socrates.addAnimation("piscando", goku);
+  socrates.addAnimation("comendo", vegeta);
+  socrates.addAnimation("triste", gohan);
+  socrates.changeAnimation("piscando");
   socrates.scale=0.3;
   aristoteles=createImg("./Imagens/cut_btn.png");
   aristoteles.position(220,30);
   aristoteles.size(50,50);
   aristoteles.mouseClicked(platao);
+  numero18=createImg("./Imagens/balloon.png");
+  numero18.position(10,200);
+  numero18.size(150,100);
+  numero18.mouseClicked(numero17);
+  titi=createImg("./Imagens/mute.png");
+  titi.position(450,20);
+  titi.size(50,50);
+  titi.mouseClicked(bulma);
+  
 
 }
 
@@ -59,14 +104,60 @@ function draw()
   Engine.update(engine);
   piso.tosemideia();
   teiadomiranha.show();
+  if(lemon !== null){
   image(magali,lemon.position.x,lemon.position.y,60,60);
+  }
+
+  if(cell(lemon,socrates) === true){
+    socrates.changeAnimation("comendo");
+    bills.play();
+  }
+
+  if(lemon !== null && lemon.position.y >= 650){
+    socrates.changeAnimation("triste");
+    mestreKame.stop();
+    raditz.play();
+    lemon=null;
+  }
   drawSprites();
 }
 
 function platao(){
+  kuririn.play();
   teiadomiranha.break();
   colabastao.invisible();
   colabastao=null;
 }
+
+function cell(melancia,coelho){
+  if(lemon !== null){
+    var freeza = dist(melancia.position.x, melancia.position.y, coelho.position.x, coelho.position.y);
+    if(freeza <= 80){
+      World.remove(engine.world, lemon);
+      lemon = null;
+      return true;
+    } else {
+      return false;
+    }
+  }
+}
+function numero17(){
+  Matter.Body.applyForce(lemon,{x:0,y:0},{x:0.01,y:0});
+  majinBoo.play();
+}
+function bulma(){
+  if(mestreKame.isPlaying()){
+    mestreKame.stop();
+  }else{
+    mestreKame.play();
+  }
+
+}
+
+
+
+
+
+
 
 
